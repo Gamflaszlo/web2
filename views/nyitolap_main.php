5c6a60cead4122b074193e3e39efd993f37e48b3
Gamflaszlo <151478117+Gamflaszlo@users.noreply.github.com>
2023-11-20 01:08:47 +0100

    Első

---
+++ views/nyitolap_main.php
@@ -0,0 +1,17 @@
+<section class = "felso"  >
+
+<br>asfdsaf<br>
+erere
+error_clear_laster
+exif_read_datar
+eachre
+<table>eachtgef
+
+tfe
+</table>
+</section>
+
+<section class= " felso2"> 
+
+ccdcfsdfdsf
+</section>
