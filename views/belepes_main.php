014321d3febf7f6c9a8c504dd027bd169d600996
Gamflaszlo <151478117+Gamflaszlo@users.noreply.github.com>
2023-11-20 09:34:57 +0100

    igazítás

--- views/belepes_main.php
+++ views/belepes_main.php
@@ -1,3 +1,4 @@
+<section class=felso>
 <h2>Belépés</h2>
 <form action="<?= SITE_ROOT ?>beleptet" method="post">
     <label for="login">Felhasználó:</label><input type="text" name="login" id="login" required pattern="[a-zA-Z][\-\.a-zA-Z0-9_]{3}[\-\.a-zA-Z0-9_]+"><br>
@@ -5,3 +6,9 @@
     <input type="submit" value="Küldés">
 </form>
 <h2><br><?= (isset($viewData['uzenet']) ? $viewData['uzenet'] : "") ?><br></h2>
+</section>
+
+<section class=felso2>
+
+dddddd
+</section>
