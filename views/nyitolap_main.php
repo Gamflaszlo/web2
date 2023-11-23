1bd2db6895487b2ab1f9537873bfffee3f9e661c
Gamflaszlo <151478117+Gamflaszlo@users.noreply.github.com>
2023-11-23 14:52:53 +0100

    nyito

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -1,21 +1,37 @@
 
 <section class = "seckinalat1"  >
     <div class= flexfo>
-<div class= piz1> 
-<img src="images/pizza-5.jpg" height=300px; width=300px;> sfsdfddfdsf
+<div class= piz2> 
+<img src="images/about.jpg" height=50%; width=50%;>
 </div>
-<div class= piz1><img src="images/pizza-8.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= pizp><pre>Rólunk
+    Amióta megismertük Itáliát nincs élet nélküle. A nyár
+    csak az olasz tenger, a pizza csak nápolyi, a tészta
+    aldente. Legyen olaszosan könnyű az élet. Jót enni, 
+    inni, veletek, mi csak ezt kívánjuk. </pre>  > </div>
 </section>
 <section class = "seckinalat2"  >
-<div class=flexfo>
-<div class= piz1><img src="images/pizza-7.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
-<div class= piz1><img src="images/pizza-6.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class=flexfo2>
+<div class= pizp2> <pre> 
+                Menü
+    
+            Autentikus ízek a legnagyobb odafigyeléssel,
+            gondosan válogatott olasz alapanyagok,
+            antipasti, primi, secondi, desszert és
+            koktélok. Hunyd be a szemed, kóstolj, és utazz
+            velünk! Találkozzunk nálunk! </pre></div>
+<div class= piz3><img  src="images/bg_2.jpg" height=60%; width=60%;></div>
 </div>
 </section>
 <section class = "seckinalat3"  >
 <div class= flexfo>
-<div class= piz1><img src="images/pizza-4.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
-<div class= piz1><img src="images/pizza-3.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= piz2><img src="images/futar.jpg" height=30%; width=30%;></div>
+<div class= pizp><pre> Házhozszállítás
+  Add le a rendelésed a 00000000 számon és
+  vidd haza a kedvenc fogásodat.
+  vagy 
+  hászhozszállításérthívd a futárt. </pre>
+</div>
 </div>
 </section>
 
