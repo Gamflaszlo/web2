04cdef065c29b38715acd42fa704174a0e5f5fe8
Gamflaszlo <151478117+Gamflaszlo@users.noreply.github.com>
2023-11-24 08:22:48 +0100

    visszaállítva

---
+++ views/kinalat_main.php
@@ -0,0 +1,26 @@
+<section class = "seckinalat"  >
+    <div class= flex>
+<div class= foimg> 
+<img src="images/pizza-5.jpg" height=300px; width=300px;> sfsdfddfdsf
+</div>
+<div class= foimg><img src="images/pizza-8.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-7.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-6.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-4.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-3.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-2.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-1.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-8.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-4.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+<div class= foimg><img src="images/pizza-5.jpg" height=300px; width=300px;> sfsdfddfdsf</div>
+
+
+
+
+</div>
+    
+
+
+
+</section>
+
