fdc85ab96b76cca86d67a6f1b8546b7027dea34b
Gamflaszlo <151478117+Gamflaszlo@users.noreply.github.com>
2023-12-20 10:40:44 +0100

    hozzaszolas

--- views/hozzaszolas_main.php
+++ views/hozzaszolas_main.php
@@ -1,23 +1,33 @@
 <section class = "felso"  >
 <p> Írd ide a kommentet</p><br/>
-<form method="post" action="komment.php">
+
+<form method="POST">
 
 </br>
-<textarea rows="10" cols="80" name="comment">
+<textarea rows="10" cols="80" name="comit">
 
 </textarea><br/>
-
+<input type="submit" name="kuld" 
+                 values="Kuld"/>
 
 </form><br/>
+<?php
 
-
+ 
+ 
+ $connection = Database::getConnection();
+ $sqlInsert = "INSERT INTO hozzaszolas(id,hozzasz) Values (0,:hozzasz)";
+ 
+                $stmt = $connection->prepare($sqlInsert); 
+                $stmt->execute(array(':hozzasz' => $_POST['Kuld'])); 
+                
+?>
 
 
 </section>
 
 <section class= " felso2"> 
 
-ccdcfsdfdsf
 </section>
 
 <section class= "also">
